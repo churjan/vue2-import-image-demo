140720d46c29d7dad75d36c38446ec903b6e1b76
Churjan <churjan@163.com>
2021-11-23 14:56:27 +0800

    Update interceptor.js

--- src/apis/interceptor.js
+++ src/apis/interceptor.js
@@ -5,7 +5,7 @@ axios.interceptors.response.use(
     console.log('interceptors then:', response);
     const { status = 0, message = '' } = response.data;
     // 默认status不为200时，全局会弹toast错误提示
-    const { isShowToast = true } = response.config.headers;
+    const { isShowToast = true } = response.config;
     if (!isShowToast) {
       return response.data;
     }
@@ -16,8 +16,8 @@ axios.interceptors.response.use(
     return Promise.reject(message);
   },
   (error) => {
-    alert(error);
-    Promise.reject(error);
+    console.log(error);
+    return Promise.reject(error);
   }
 );
 export default axios;
